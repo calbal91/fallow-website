23eb274f1443acccf7fbc4bb35dc7bc02e0ebeba
Callum Ballard <callumjballard@gmail.com>
2020-05-09 16:56:00 +0100

    Started work on info pages

--- static/script.js
+++ static/script.js
@@ -1,12 +1,14 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-/* Open the sidenav */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
-  document.getElementById("myHamburger").style.opacity = "0"; 
-}
-
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("myHamburger").style.opacity = "1"; 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
